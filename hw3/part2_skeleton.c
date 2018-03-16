5e3b6504d03576d563ab0415b1fff7446daaccab
Simon Zheng <simz97@outlook.com>
2018-03-16 03:44:27 -0400

    HW3 Some linting and styling

--- hw3/part2_skeleton.c
+++ hw3/part2_skeleton.c
@@ -10,12 +10,13 @@
 //use the qsort function 
 // an argument to qsort function is a function that compares 2 quantities
 //use this there.
-int cmpfunc (const void * a, const void * b) {
-   return ( *(int*)a - *(int*)b );
+int cmpfunc (const void* a, const void* b)
+{
+   return *(int*)a - *(int*)b;
 }
 
 //function to swap 2 integers
-void swap(int *a, int *b)
+void swap(int* a, int* b)
 {
     if (*a != *b)
     {
@@ -27,13 +28,13 @@ void swap(int *a, int *b)
 }
 
 //Prints the sequence and the performance metric
-void printSeqNPerformance(int *request, int numRequest)
+void printSeqNPerformance(int* request, int numRequest)
 {
-    int i, last, acc = 0;
-    last = START;
-    printf("\n");
+    int acc = 0;
+    int last = START;
+    puts("");
     printf("%d", START);
-    for (i = 0; i < numRequest; i++)
+    for (int i = 0; i < numRequest; i++)
     {
         printf(" -> %d", request[i]);
         acc += abs(last - request[i]);
@@ -44,75 +45,76 @@ void printSeqNPerformance(int *request, int numRequest)
 }
 
 //access the disk location in FCFS
-void accessFCFS(int *request, int numRequest)
+void accessFCFS(int* request, int numRequest)
 {
     //simplest part of assignment
-    printf("\n----------------\n");
+    puts("\n----------------");
     printf("FCFS :");
     printSeqNPerformance(request, numRequest);
-    printf("----------------\n");
+    puts("----------------");
     return;
 }
 
 //access the disk location in SSTF
-void accessSSTF(int *request, int numRequest)
+void accessSSTF(int* request, int numRequest)
 {
     //write your logic here
-    printf("\n----------------\n");
+    puts("\n----------------");
     printf("SSTF :");
     printSeqNPerformance(request, numRequest);
-    printf("----------------\n");
+    puts("----------------");
     return;
 }
 
 //access the disk location in SCAN
-void accessSCAN(int *request, int numRequest)
+void accessSCAN(int* request, int numRequest)
 {
     
 	//write your logic here
-    printf("\n----------------\n");
+    puts("\n----------------");
     printf("SCAN :");
-    printSeqNPerformance(newRequest, newCnt);
-    printf("----------------\n");
+    // printSeqNPerformance(newRequest, newCnt);
+    puts("----------------");
     return;
 }
 
 //access the disk location in CSCAN
-void accessCSCAN(int *request, int numRequest)
+void accessCSCAN(int* request, int numRequest)
 {
     //write your logic here
-    printf("\n----------------\n");
+    puts("\n----------------");
     printf("CSCAN :");
-    printSeqNPerformance(newRequest, newCnt);
-    printf("----------------\n");
+    // printSeqNPerformance(newRequest, newCnt);
+    puts("----------------");
     return;
 }
 
 //access the disk location in LOOK
-void accessLOOK(int *request, int numRequest)
+void accessLOOK(int* request, int numRequest)
 {
     //write your logic here
-    printf("\n----------------\n");
+    puts("\n----------------");
     printf("LOOK :");
-    printSeqNPerformance(newRequest, newCnt);
-    printf("----------------\n");
+    // printSeqNPerformance(newRequest, newCnt);
+    puts("----------------");
     return;
 }
 
 //access the disk location in CLOOK
-void accessCLOOK(int *request, int numRequest)
+void accessCLOOK(int* request, int numRequest)
 {
     //write your logic here
-    printf("\n----------------\n");
+    puts("\n----------------");
     printf("CLOOK :");
-    printSeqNPerformance(newRequest,newCnt);
-    printf("----------------\n");
+    // printSeqNPerformance(newRequest,newCnt);
+    puts("----------------");
     return;
 }
 
 int main()
 {
-    int *request, numRequest, i,ans;
+    int* request;
+    int numRequest, ans;
 
     //allocate memory to store requests
     printf("Enter the number of disk access requests : ");
@@ -120,21 +122,21 @@ int main()
     request = malloc(numRequest * sizeof(int));
 
     printf("Enter the requests ranging between %d and %d\n", LOW, HIGH);
-    for (i = 0; i < numRequest; i++)
+    for (int i = 0; i < numRequest; i++)
     {
         scanf("%d", &request[i]);
     }
 
-    printf("\nSelect the policy : \n");
-    printf("----------------\n");
-    printf("1\t FCFS\n");
-    printf("2\t SSTF\n");
-    printf("3\t SCAN\n");
-    printf("4\t CSCAN\n");
-    printf("5\t LOOK\n");
-    printf("6\t CLOOK\n");
-    printf("----------------\n");
-    scanf("%d",&ans);
+    puts("\nSelect the policy : ");
+    puts("----------------");
+    puts("1\t FCFS");
+    puts("2\t SSTF");
+    puts("3\t SCAN");
+    puts("4\t CSCAN");
+    puts("5\t LOOK");
+    puts("6\t CLOOK");
+    puts("----------------");
+    scanf("%d", &ans);
 
     switch (ans)
     {
@@ -146,11 +148,11 @@ int main()
     case 2: accessSSTF(request, numRequest);
         break;
 
-        //access the disk location in SCAN
+    //access the disk location in SCAN
      case 3: accessSCAN(request, numRequest);
         break;
 
-        //access the disk location in CSCAN
+    //access the disk location in CSCAN
     case 4: accessCSCAN(request,numRequest);
         break;
 
@@ -165,5 +167,5 @@ int main()
     default:
         break;
     }
-    return 0;
+    return EXIT_SUCCESS;
 }
