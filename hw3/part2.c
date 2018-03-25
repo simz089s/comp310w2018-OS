08de7a266202053edf3cc4fb099a139b6d271cf3
Simon Zheng <simz97@outlook.com>
2018-03-24 22:35:46 -0400

    HW3 Implement SSTF

--- hw3/part2.c
+++ hw3/part2.c
@@ -1,6 +1,17 @@
+/**
+ * Student name : Simon Zheng
+ * Student ID   : 260744353
+ */
+
+// TODO: REMOVE TEST CODE!!!
+
 #include <stdio.h>  //for printf and scanf
 #include <stdlib.h> //for malloc
 #include <limits.h>
+
+#ifndef __INT_MAX__
+#define __INT_MAX__ (-((1 << (sizeof(int)*CHAR_BIT-1))+1))
+#endif
 
 #define LOW 0
 #define HIGH 199
@@ -59,30 +70,45 @@ void accessFCFS(int* request, int numRequest)
     return;
 }
 
+// Interval length between two integers function
+int dist(int a, int b) { return abs(a - b); }
+
 //access the disk location in SSTF
 void accessSSTF(int* request, int numRequest)
 {
     //write your logic here
-    int nearest = 0;
+    /**
+     * Pick direction of closest request to START
+     * If greater, sort all numbers greater than START to righthand side
+     * and descending order for requests smaller than START to lefthand side
+     * Vice-versa if starting request is smaller
+     *
+     * Here I just used a naive bruteforce algorithm that finds the closest
+     * element right of the current one going from left to right (smaller to
+     * bigger) index in the array
+     */
     int nearestDist = abs(request[0] - START);
-    for (int i = 0; i < numRequest; i++)
+    for (int i = 1; i < numRequest; i++)
     {
         int distance = abs(request[i] - START);
-        nearest = distance < nearestDist ? i : nearest;
-        nearestDist = request[nearest];
+        if (distance < nearestDist)
+        {
+            nearestDist = distance;
+            swap(&request[0], &request[i]);
+        }
     }
-    swap(&request[0], &request[nearest]);
     for (int i = 0; i < numRequest; i++)
     {
+        nearestDist = INT_MAX;
         for (int j = i+1; j < numRequest; j++)
         {
             int distance = abs(request[i] - request[j]);
-            nearest = distance < nearestDist ? j : nearest;
-            nearestDist = request[nearest];
+            if (distance < nearestDist)
+            {
+                nearestDist = distance;
+                swap(&request[j], &request[i+1]);
+            }
         }
-        swap(&request[i], &request[nearest]);
-        nearest = -1;
-        nearestDist = INT_MAX;
     }
     puts("\n----------------");
     printf("SSTF :");
@@ -138,19 +164,23 @@ void accessCLOOK(int* request, int numRequest)
 
 int main()
 {
-    int* request;
+    // int* request;
     int numRequest, ans;
 
     //allocate memory to store requests
     printf("Enter the number of disk access requests : ");
-    scanf("%d", &numRequest);
-    request = malloc(numRequest * sizeof(int));
+    // scanf("%d", &numRequest);
+    // request = malloc(numRequest * sizeof(int));
 
     printf("Enter the requests ranging between %d and %d\n", LOW, HIGH);
-    for (int i = 0; i < numRequest; i++)
+    // for (int i = 0; i < numRequest; i++)
     {
-        scanf("%d", &request[i]);
+        // scanf("%d", &request[i]);
     }
+
+    // TEST (should be 53->54->55->52->59->10->9->8->7->6->1)
+    int request[] = {59,54,55,52,1,10,6,7,9,8};
+    numRequest = sizeof(request)/sizeof(typeof(request[0]));
 
     puts("\nSelect the policy : ");
     puts("----------------");
