6397845a954030589f5ccd269fe9518efbd8ab50
Simon Zheng <simz97@outlook.com>
2018-04-15 22:57:57 -0400

    HW3 Pause

--- hw3/bankers.c
+++ hw3/bankers.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <inttypes.h>
+#include <string.h>
 #include <pthread.h>
 #include <semaphore.h>
 
-int main(int argc, char* argv[argc])
+int main(/*int argc, char* argv[argc]*/)
 {
+    int numProc, numRes;
+
+    printf("Enter number of processes: ");
+    if (scanf("%d", &numProc) < 1) { return EXIT_FAILURE; }
+
+    printf("\nEnter number of resources: ");
+    if (scanf("%d", &numRes) < 1) { return EXIT_FAILURE; }
+
+    int availRes[4096/sizeof(int)] = {0,};
+    while ()
+
+    int resources[distRes][2];
+    for (int i = 0; i < distRes; i++)
+    {
+        int  = strtoimax(argv[2], (char**)NULL, 10);
+    }
     return EXIT_SUCCESS;
 }
